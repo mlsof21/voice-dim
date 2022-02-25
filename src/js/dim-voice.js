8d83bae9f6c140069fc2784772d69df8af63084f
mlsof21 <matthew.sherrill.2107@gmail.com>
2022-02-25 07:49:33 -0600

    add loadout equip, max power equip - ms

--- src/js/dim-voice.js
+++ src/js/dim-voice.js
@@ -1,10 +1,8 @@
 // get html elements
 let searchBar = document.getElementsByName('filter').length > 0 ? document.getElementsByName('filter')[0] : null;
-let dblClick = new Event('dblclick', {
-  bubbles: true,
-  cancelable: true,
-  view: window
-});
+
+let singleClick = new Event('click', { bubbles: true, cancelable: true, view: window });
+let dblClick = new Event('dblclick', { bubbles: true, cancelable: true, view: window });
 
 let inputEvent = new Event("input", { bubbles: true });
 let enterEvent = new KeyboardEvent("keydown", { bubbles: true, which: 13, key: "enter" });
@@ -29,28 +27,31 @@ function setNativeValue(element, value) {
 
 
 const weaponTypeQueries = {
-  "auto rifle": "is:autorifle",
-  "autorifle": "is:autorifle",
-  "hand cannon": "is:handcannon",
-  "pulse rifle": "is:pulserifle",
-  "scout rifle": "is:scoutrifle",
-  "sidearm": "is:sidearm",
-  "submachine gun": "is:submachinegun",
-  "bow": "is:bow",
-  "shotgun": "is:shotgun",
-  "sniper rifle": "is:sniperrifle",
-  "fusion rifle": "is:fusionrifle",
-  "trace rifle": "is:tracerifle",
-  "grenade launcher": "is:grenadelauncher",
-  "rocket launcher": "is:rocketlauncher",
-  "linear fusion rifle": "is:linearfusionrifle",
-  "machinegun": "is:machinegun",
-  "machine gun": "is:machinegun",
-  "sword": "is:sword",
+  "auto rifle": "is:weapon is:autorifle",
+  "autorifle": "is:weapon is:autorifle",
+  "hand cannon": "is:weapon is:handcannon",
+  "handcannon": "is:weapon is:handcannon",
+  "pulse rifle": "is:weapon is:pulserifle",
+  "scout rifle": "is:weapon is:scoutrifle",
+  "sidearm": "is:weapon is:sidearm",
+  "submachine gun": "is:weapon is:submachinegun",
+  "bow": "is:weapon is:bow",
+  "shotgun": "is:weapon is:shotgun",
+  "sniper rifle": "is:weapon is:sniperrifle",
+  "fusion rifle": "is:weapon is:fusionrifle",
+  "trace rifle": "is:weapon is:tracerifle",
+  "grenade launcher": "is:weapon is:grenadelauncher",
+  "rocket launcher": "is:weapon is:rocketlauncher",
+  "linear fusion rifle": "is:weapon is:linearfusionrifle",
+  "machinegun": "is:weapon is:machinegun",
+  "machine gun": "is:weapon is:machinegun",
+  "sword": "is:weapon is:sword",
+  "glaive": "is:weapon is:glaive", // doesn't currently work, no search filter in DIM
 }
 
 const energyTypeQueries = {
   "arc": "is:arc",
+  "ark": "is:arc",
   "solar": "is:solar",
   "void": "is:void",
   "stasis": "is:stasis",
@@ -63,57 +64,14 @@ const weaponSlotQueries = {
   "heavy": "is:power"
 }
 
-function parseSpeech(transcript) {
-  console.log("parsing", transcript);
-  let query = transcript.substring(3).trim();
-  if (query.indexOf('transfer') === 0) {
-    query = query.substring(8).toLowerCase().trim();
-    // const weaponQueries = Object.keys(weaponTypeQueries);
-    // const energyQueries = Object.keys(energyTypeQueries);
-    // const slotQueries = Object.keys(weaponSlotQueries);
-
-    const splitQuery = query.split(' ');
-    let fullQuery = '';
-    for (const weaponType of Object.keys(weaponTypeQueries)) {
-      if (query.includes(weaponType)) {
-        fullQuery += weaponTypeQueries[weaponType] + " ";
-        break;
-      }
-    }
-
-    for (const energyType of Object.keys(energyTypeQueries)) {
-      const search = `\\b${energyType}\\b`;
-      const re = new RegExp(search, 'g');
-      if (query.search(re) >= 0) {
-        fullQuery += energyTypeQueries[energyType];
-        break;
-      }
-    }
-
-    for (const slotType of Object.keys(weaponSlotQueries)) {
-      if (query.includes(slotType)) {
-        fullQuery += weaponSlotQueries[slotType] + " ";
-        break;
-      }
-    }
-
-    fullQuery = fullQuery.trim();
-
-    if (fullQuery === "") {
-      const availableItems = getAllTransferableItems();
-      const itemToGet = getClosestMatch(availableItems, query);
-      const itemDiv = document.querySelector(`[title*="${itemToGet}" i`);
-      if (itemDiv) {
-        itemDiv.dispatchEvent(dblClick);
-      }
-    }
-    else {
-      console.log("Full query being sent to DIM: " + fullQuery);
-      transferByWeaponTypeQuery(fullQuery);
-    }
-
-  }
-
+const armorTypeQueries = {
+  "helmet": "is:armor is:helmet",
+  "arms": "is:armor is:gauntlets",
+  "gauntlets": "is:armor is:gauntlets",
+  "chest": "is:armor is:chest",
+  "legs": "is:armor is:leg",
+  "boots": "is:armor is:leg",
+  "leg": "is:armor is:leg",
 }
 
 const transferableItemClassNames = [
@@ -127,13 +85,103 @@ const transferableItemClassNames = [
   "ClassItem"
 ]
 
+function parseSpeech(transcript) {
+  console.log("parsing", transcript);
+  let query = transcript.substring(3).trim();
+  if (query.indexOf('transfer') > -1) {
+    handleItemTypeQuery(query.replace('transfer', '').toLowerCase().trim());
+  } else if (query.indexOf("start farming") > -1) {
+    handleStartFarmingMode();
+  } else if (query.indexOf("stop farming") > -1) {
+    handleStopFarmingMode();
+  } else if (query.indexOf('equip max power') > -1) {
+    handleEquipMaxPower();
+  } else if (query.indexOf('equip loadout') > -1 || query.indexOf('equip load out') > -1) {
+    handleEquipLoadout(query.replace(/equip load[\s]?out/, '').trim());
+  }
+
+
+}
+
+
+function handleItemTypeQuery(query) {
+  let fullQuery = '';
+
+  const genericQueries = [weaponTypeQueries, energyTypeQueries, weaponSlotQueries, armorTypeQueries];
+
+  for (const genericQuery of genericQueries) {
+    fullQuery += checkForGenericTerms(genericQuery, query);
+  }
+
+
+  fullQuery = fullQuery.trim();
+
+  if (fullQuery === "") {
+    const availableItems = getAllTransferableItems();
+    const itemToGet = getClosestMatch(Object.keys(availableItems), query);
+    const itemToGetMapped = availableItems[itemToGet.item];
+    const itemDiv = document.querySelector(`[title^="${itemToGetMapped}" i`);
+    if (itemDiv) {
+      itemDiv.dispatchEvent(dblClick);
+    }
+  }
+  else {
+    console.log("Full query being sent to DIM: " + fullQuery);
+    transferByWeaponTypeQuery(fullQuery);
+  }
+  return query;
+}
+
+function handleStartFarmingMode() {
+  const currentCharacter = document.querySelector('.character.current');
+  currentCharacter.dispatchEvent(singleClick);
+  const farmingSpan = [...document.querySelectorAll('.loadout-menu span')].filter(x => x.textContent.includes("Farming"))[0];
+  farmingSpan.dispatchEvent(singleClick);
+}
+
+function handleStopFarmingMode() {
+  const stopButton = document.querySelector('#item-farming button');
+  stopButton.dispatchEvent(singleClick);
+}
+
+function handleEquipMaxPower() {
+  const currentCharacter = document.querySelector('.character.current');
+  currentCharacter.dispatchEvent(singleClick);
+  const maxPowerSpan = [...document.querySelectorAll('.loadout-menu span')].filter(x => x.textContent.includes("Max Power"))[0];
+  maxPowerSpan.dispatchEvent(singleClick);
+}
+
+function handleEquipLoadout(loadoutName) {
+  const currentCharacter = document.querySelector('.character.current');
+  currentCharacter.dispatchEvent(singleClick);
+  const availableLoadoutNames = [...document.querySelectorAll('.loadout-menu li > span[title]:first-child')].map(x => x.textContent);
+  const loadoutResult = getClosestMatch(availableLoadoutNames, loadoutName);
+  const loadoutToEquip = loadoutResult.item;
+  const loadoutToEquipSpan = document.querySelector(`.loadout-menu span[title="${loadoutToEquip}"]`);
+  loadoutToEquipSpan.dispatchEvent(singleClick);
+}
+
+function checkForGenericTerms(queries, query) {
+  let fullQuery = "";
+  for (const type of Object.keys(queries)) {
+    const search = `\\b${type}\\b`;
+    const re = new RegExp(search, 'g');
+    if (query.search(re) >= 0) {
+      fullQuery += queries[type] + " ";
+      break;
+    }
+  }
+  return fullQuery;
+}
+
 function getAllTransferableItems() {
-  const items = [];
+  const items = {};
   for (const className of transferableItemClassNames) {
     const result = document.querySelectorAll(`.item-type-${className} .item`);
     result.forEach(item => {
       const split = item.title.split('\n');
-      items.push(split[0]);
+      const sanitized = split[0].replaceAll('.', '');
+      items[sanitized] = split[0];
     });
   }
 
@@ -149,7 +197,7 @@ function getClosestMatch(availableItems, query) {
 
   const result = fuse.search(query);
   console.log({ result, query });
-  return result.length > 0 ? result[0].item : "";
+  return result.length > 0 ? result[0] : "";
 }
 
 function transferByWeaponTypeQuery(searchInput) {
@@ -171,9 +219,11 @@ function transferByWeaponTypeQuery(searchInput) {
         filteredItems[0].dispatchEvent(dblClick);
       }
     }
-    const actions = [{ func: inputFunc, timeout: 500 }, { func: enterFunc, timeout: 500 }, {
-      func: transferFunc, timeout: 2000
-    }];
+    const actions = [
+      { func: inputFunc, timeout: 500 },
+      { func: enterFunc, timeout: 500 },
+      { func: transferFunc, timeout: 2000 }
+    ];
 
     let totalTimeout = 0;
     for (let i = 0; i < actions.length; i++) {
@@ -208,7 +258,7 @@ recognition.onresult = e => {
   // console.log({e});
   var transcripts = [].concat.apply([], [...e.results].map(res => [...res].map(alt => alt.transcript)));
   if (transcripts.some(t => t.indexOf(magic_word) > -1)) {
-    const fullTranscript = transcripts.join('');
+    const fullTranscript = transcripts.join('').toLowerCase();
     parseSpeech(fullTranscript);
     stopSpeech();
   }
