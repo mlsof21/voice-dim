ed5501a408c12a741984e6ffaa8dc3c8a4fa2f14
Matthew Sherrill <matthew.sherrill.2107@gmail.com>
2022-08-12 09:05:58 -0600

    updated build script, manifest, and icons -ms

--- build.py
+++ build.py
@@ -1,12 +1,18 @@
-import os, zipfile, re, json, os.path
+import os
+import zipfile
+import re
+import json
+import os.path
 from typing import List
+
 
 def get_files_to_zip():
     zippable_files = []
     exclude = [
-        r'\.(py|md|zip)', # file extensions
-        r'\.gitignore|package\.json|package-lock\.json|tsconfig\.json', # files
-        r'(\\|/)(screenshots|test|node_modules|\.github|\.git)' # directories    
+        r'\.(py|md|zip|ts)',  # file extensions
+        r'\..*ignore|package\.json|package-lock\.json|tsconfig\.json|\.prettierrc\.json|requirements\.txt',  # files
+        # directories
+        r'(\\|/)(screenshots|test|node_modules|\.github|\.git|env|\.vscode)'
     ]
 
     for root, folders, files in os.walk('.'):
@@ -25,7 +31,7 @@ def zip_files(files: List[str], browser: str):
 
     output_file = os.path.join(output_folder, f"dim-voice-{browser}.zip")
     zf = zipfile.ZipFile(output_file, 'w', zipfile.ZIP_STORED)
-    
+
     for f in files:
         print("Creating for", browser)
         if f.endswith("manifest.json"):
@@ -34,7 +40,7 @@ def zip_files(files: List[str], browser: str):
                 manifest["manifest_version"] = 2
                 del manifest['action']
                 # manifest["permissions"].append("https://www.bungie.net/*")
-            
+
             zf.writestr(f[2:], json.dumps(manifest, indent=2))
         else:
             zf.write(f[2:])
@@ -46,6 +52,6 @@ if __name__ == "__main__":
     browsers = ["chrome", "firefox"]
     files_to_zip = get_files_to_zip()
     print("Files to zip:", files_to_zip)
-    
+
     for browser in browsers:
         zip_files(files_to_zip, browser)
