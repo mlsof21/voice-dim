9c638fb0a11833568dc2989c10a48af4d3d9f18b
Matthew Sherrill <matthew.sherrill.2107@gmail.com>
2022-08-21 21:51:19 -0600

    default the shortcut correctly, minify fuse, update build script, update version - ms

--- scripts/build.py
+++ scripts/build.py
@@ -1,8 +1,8 @@
-import os
-import zipfile
-import re
 import json
+import os
 import os.path
+import re
+import zipfile
 from typing import List
 
 
@@ -10,9 +10,9 @@ def get_files_to_zip():
     zippable_files = []
     exclude = [
         r'\.(py|md|zip|ts)',  # file extensions
-        r'\..*ignore|package\.json|package-lock\.json|tsconfig\.json|\.prettierrc\.json|requirements\.txt',  # files
+        r'\..*ignore|package\.json|package-lock\.json|tsconfig\.json|\.prettierrc\.json|requirements\.txt|webpack.*',  # files
         # directories
-        r'(\\|/)(screenshots|test|node_modules|\.github|\.git|env|\.vscode)'
+        r'(\\|/)(screenshots|test|node_modules|\.github|\.git|env|\.vscode|build)'
     ]
 
     for root, folders, files in os.walk('.'):
@@ -29,7 +29,17 @@ def zip_files(files: List[str], browser: str):
     if not os.path.isdir(output_folder):
         os.mkdir(output_folder)
 
-    output_file = os.path.join(output_folder, f"dim-voice-{browser}.zip")
+    version = json.load(open('manifest.json'))['version']
+    output_file = os.path.join(
+        output_folder, f"dim-voice-{browser}-{version}.zip")
+
+    if os.path.exists(output_file):
+        user_response = input(
+            f"Zip file {output_file} already exists. Do you want to continue? (yn)")
+        if user_response in ("n", "no", "N"):
+            exit(1)
+        else:
+            print(f"Overwriting existing {output_file}")
     zf = zipfile.ZipFile(output_file, 'w', zipfile.ZIP_STORED)
 
     for f in files:
